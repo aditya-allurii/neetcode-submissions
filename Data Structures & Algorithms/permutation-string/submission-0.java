0b9d0b5ca30fea2c9b94502587711de9782b7fb1
Alluri Aditya <adityaalluri110@gmail.com>
2026-07-04 14:34:04 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] s1count = new int[26];
+        int[] windowcount = new int[26];
+        int left = 0;
+        for(int i = 0;i<s1.length();i++){
+            s1count[s1.charAt(i) - 'a']++;
+        }
+        for(int right = 0;right < s2.length();right++){
+            windowcount[s2.charAt(right) - 'a']++;
+            if(right - left + 1 > s1.length()){
+                windowcount[s2.charAt(left) - 'a']--;
+                left++;
+            }
+            if(s1.length() == right - left + 1){
+                if(Arrays.equals(s1count,windowcount)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
