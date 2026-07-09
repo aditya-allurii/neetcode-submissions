52b9d3958c472c1699dd1ae2a296d2304233a604
Alluri Aditya <adityaalluri110@gmail.com>
2026-07-09 19:20:32 +0530

    Add: first-missing-positive - submission-2

---
+++ Data Structures & Algorithms/first-missing-positive/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        //optimal
+        int n = nums.length;
+        int i = 0;
+        while(i  < n){
+            int correct = nums[i] - 1;
+            if(nums[i] > 0 && nums[i] <= n && nums[i] != nums[correct]){
+                //swapp
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }else {
+                i++;
+            }
+        }
+        for(i = 0;i < n;i++){
+            if(nums[i] != i+1){
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+}
