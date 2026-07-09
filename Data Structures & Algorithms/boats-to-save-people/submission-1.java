c30a04d0e3d25b91b05919015dbea18c7fbb7691
Alluri Aditya <adityaalluri110@gmail.com>
2026-07-09 19:26:18 +0530

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length-1;
+        int ans = 0;
+        while(left <= right){
+            int maxweight = people[left] + people[right];
+            if(maxweight <= limit){
+                left++;
+                right--;
+                ans++;
+                //or simply only left++
+            }else {
+                right--;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
