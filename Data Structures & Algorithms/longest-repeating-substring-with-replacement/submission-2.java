71730f1b6426657593e507e5940607b5e4db2b37
Alluri Aditya <adityaalluri110@gmail.com>
2026-07-10 07:02:40 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int left = 0;
+        int maxfreq = 0;
+        int maxlen = 0;
+        int[] freq = new int[26];
+        for(int i = 0;i < s.length();i++){
+            freq[s.charAt(i) - 'A']++;
+            maxfreq = Math.max(maxfreq,freq[s.charAt(i) - 'A']);
+
+            while(i - left + 1 - maxfreq > k){
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+            maxlen = Math.max(maxlen,i - left + 1);
+        }
+        return maxlen;
+    }
+}
