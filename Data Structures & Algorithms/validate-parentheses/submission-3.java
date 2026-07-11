f18e7159d62cf9299b663071e09e0a3df54ef47c
Alluri Aditya <adityaalluri110@gmail.com>
2026-07-11 08:38:33 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }else {
+                if(stack.isEmpty()){
+                    return false;
+                }
+                if(ch == ')' && stack.pop() != '('){
+                    return false;
+                }
+                if(ch == '}' && stack.pop() != '{'){
+                    return false;
+                }
+                if(ch == ']' && stack.pop() != '['){
+                    return false;
+                }
+                //stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
