d80830db0d08729eea293c069586e9daeb23bf18
Alluri Aditya <adityaalluri110@gmail.com>
2026-07-27 07:35:45 +0530

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        List<Integer> list = new ArrayList<>();
+        for(ListNode head : lists){
+            while(head != null){
+                list.add(head.val);
+                head = head.next;
+            }
+        }
+        Collections.sort(list);
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        for(int num : list){
+            current.next = new ListNode(num);
+            current = current.next;
+        }
+        return dummy.next;
+    }
+}
